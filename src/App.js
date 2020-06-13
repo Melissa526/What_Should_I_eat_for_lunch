1069e060ae723c07d5ca93860e785be6e225a644
Melissa526 <jordan.hans7@gmail.com>
2020-06-13 15:17:55 +0900

    Add component style dir

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Main extends Component{
+    render() {
+        return(
+            <div className="">
+                <div className=""></div>
+            </div>
+        );
+    }
+}
+
+export default Main;
