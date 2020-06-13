972536b2efa811ea8df20f2c66ea8010a8f94443
Melissa526 <jordan.hans7@gmail.com>
2020-06-13 14:51:35 +0900

    Add directory for router and components

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Nav extends Component{
+    render() {
+        return(
+            <div>
+             Navigator Area
+            </div>
+        );
+    }
+}
+
+
+export default Nav;
