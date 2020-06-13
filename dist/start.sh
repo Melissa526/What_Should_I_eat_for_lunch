51e163e72f950c7dab49bc4db574aa03307ece78
Melissa526 <lotus526@naver.com>
2020-06-13 14:02:22 +0900

    clear repository

--- dist/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pm2 reload app.js;
-echo 'Reload pm2 demon..';
-sleep 1;
-sudo service ngix restart;
-echo 'restart nginx restart';
-echo 'All Done!';
-exit;
--- dist/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pm2 stop app.js;
-echo 'Stop app.js by pm2';
-sleep 1;
-sudo service nginx stop;
-echo 'Stop nginx server...';
-echo 'All Done!';
-exit;
