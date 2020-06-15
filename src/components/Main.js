8ff07c8f724c10a44a1864795ab1329893a5e613
Melissa526 <jordan.hans7@gmail.com>
2020-06-15 15:39:11 +0900

    add style to Nav and set toggle menu

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react'
 
+import './style/main.css';
+
 class Main extends Component{
     render() {
         return(
-            <div className="">
-                <div className=""></div>
+            <div className="main_area">
+
+                <div className="main_item_wrapper" id="korean">
+                    <img src="../img/korean_01.jpg" alt="korean" />
+                    <div className="darkness"> </div>
+                    <div className="btn-plus"><span draggable="false">+</span></div>
+                </div>
+
+                <div className="main_item_wrapper" id="western">
+                    <img src="../img/korean_01.jpg" alt="western" />
+                    <div className="darkness"> </div>
+                    <div className="btn-plus"><span draggable="false">+</span></div>
+                </div>
+
+                <div className="main_item_wrapper" id="chinese">
+                    <img src="../img/korean_01.jpg" alt="chinese" />
+                    <div className="darkness"> </div>
+                    <div className="btn-plus"><span draggable="false">+</span></div>
+                </div>
+
+                <div className="main_item_wrapper" id="japanese">
+                    <img src="../img/korean_01.jpg" alt="japanese" />
+                    <div className="darkness"> </div>
+                    <div className="btn-plus"><span draggable="false">+</span></div>
+                </div>
+
+                <div className="main_item_wrapper" id="bunsik">
+                    <img src="../img/bunsik_01.jpg" alt="bunsik" />
+                    <div className="darkness"> </div>
+                    <div className="btn-plus"><span draggable="false">+</span></div>
+                </div>
+
             </div>
         );
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Nav extends Component{
-    render() {
-        return(
-            <div>
-             Navigator Area
-            </div>
-        );
+import './style/main.css';
+import { fas, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+class Nav extends Component {
+
+    toggleNav() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
     }
-}
 
+
+   render() {
+       return(
+           <div className="topnav">
+               <a href="#home" className="active">오점뭐?</a>
+               <div id="myLinks">
+                   <a href="#news">Home</a>
+                   <a href="#map">주변탐색</a>
+                   <a href="#board">게시판</a>
+                   <a href="#about">About</a>
+               </div>
+               <a href="javascript:void(0);" className="icon" onClick={this.toggleNav}>
+
+                   <FontAwesomeIcon icon={faBars} size="2x"/>
+               </a>
+           </div>
+       );
+   }
+}
 
 export default Nav;
