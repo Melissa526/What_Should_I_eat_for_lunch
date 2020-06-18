0a70c706836553c2a9af1859c58b2178bc6c4d03
Melissa526 <jordan.hans7@gmail.com>
2020-06-18 17:10:07 +0900

    Main.js 영역 설명 추가

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-const express = require('express')
+'use strict';
+const express = require('express');
 const cors = require('cors');
-const app = express()
-const port = 3001
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
