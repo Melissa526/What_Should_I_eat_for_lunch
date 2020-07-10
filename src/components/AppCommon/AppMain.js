d03e93c77be16f6ebc09eb5e963f5ae05d50f9d7
Melissa526 <jordan.hans7@gmail.com>
2020-07-10 17:52:37 +0900

    add click event

--- src/components/AppCommon/AppMain.js
+++ src/components/AppCommon/AppMain.js
@@ -9,6 +9,22 @@ import '../../lib/css/bootstrap-reboot.css';
 import '../../lib/css/magnific-popup.css';
 
 class AppManin extends Component{
+
+    state = {
+        firstBlock : null,
+        secondBlock : null,
+        selection : null
+    }
+
+
+
+    selectFirstBlockHandler = ()=>{
+        this.setState(state => ({
+            firstBlock : 'test'
+        }));
+
+    }
+
     render() {
         return(
             <div>
@@ -74,7 +90,7 @@ class AppManin extends Component{
                                         <p className="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor
                                             sit amet, consectetur adipisicing elit. Sapiente et sed quasi.</p>
                                         <p data-aos="fade-up" data-aos-delay="200">
-                                            <a href="#" className="btn btn-white btn-outline-white">Get Started</a>
+                                            <a href="#first-selection" className="btn btn-white btn-outline-white">Get Started</a>
                                         </p>
                                     </div>
                                 </div>
@@ -91,7 +107,7 @@ class AppManin extends Component{
                     <div className="container">
                         <div className="row">
                             <div className="col-md-6 mb-4 mb-lg-0 col-lg-3 text-left service-block" data-aos="fade-up"
-                                 data-aos-delay="">
+                                 data-aos-delay="" onClick={this.selectFirstBlockHandler}>
                                 <span className="wrap-icon"><span className="flaticon-dinner d-block mb-4"></span></span>
                                 <h3 className="mb-2 text-primary">Enjoy Eating</h3>
                                 <p>A small river named Duden flows by their place and supplies it with the necessary
@@ -125,138 +141,185 @@ class AppManin extends Component{
 
 
                 {/* 두번째 메뉴선택 [s] */}
+                {(this.state.firstBlock)?
+                    <section className="section bg-light py-5  bottom-slant-gray" id="second-selection">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6 mb-4 mb-lg-0 col-lg-3 text-left service-block" data-aos="fade-up"
+                                     data-aos-delay="" onClick={this.selectFirstBlockHandler}>
+                                    <span className="wrap-icon"><span className="flaticon-dinner d-block mb-4"></span></span>
+                                    <h3 className="mb-2 text-primary">Seco</h3>
+                                    <p>A small river named Duden flows by their place and supplies it with the necessary
+                                        regelialia.</p>
+                                </div>
+                                <div className="col-md-6 mb-4 mb-lg-0 col-lg-3 text-left service-block" data-aos="fade-up"
+                                     data-aos-delay="100">
+                                    <span className="wrap-icon"><span className="flaticon-fish d-block mb-4"></span></span>
+                                    <h3 className="mb-2 text-primary">Fresh Sea Foods</h3>
+                                    <p>A small river named Duden flows by their place and supplies it with the necessary
+                                        regelialia.</p>
+                                </div>
+                                <div className="col-md-6 mb-4 mb-lg-0 col-lg-3 text-left service-block" data-aos="fade-up"
+                                     data-aos-delay="200">
+                                <span className="wrap-icon"><span
+                                    className="flaticon-hot-coffee-rounded-cup-on-a-plate-from-side-view d-block mb-4"></span></span>
+                                    <h3 className="mb-2 text-primary">Cup of Coffees</h3>
+                                    <p>A small river named Duden flows by their place and supplies it with the necessary
+                                        regelialia.</p>
+                                </div>
+                                <div className="col-md-6 mb-4 mb-lg-0 col-lg-3 text-left service-block" data-aos="fade-up" data-aos-delay="300">
+                                    <span className="wrap-icon"><span className="flaticon-meat d-block mb-4"></span></span>
+                                    <h3 className="mb-2 text-primary">Meat Eaters</h3>
+                                    <p>A small river named Duden flows by their place and supplies it with the necessary
+                                        regelialia.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    : null}
 
                 {/* 두번째 메뉴선택 [e] */}
 
 
                 {/* 메뉴선택 결과 [s] */}
-                <section className="section bg-light  top-slant-white bottom-slant-gray">
+                {(this.state.secondBlock) ?
+                    <section className="section bg-light  top-slant-white bottom-slant-gray">
 
-                    <div className="clearfix mb-5 pb-5">
-                        <div className="container-fluid">
-                            <div className="row" data-aos="fade">
-                                <div className="col-md-12 text-center heading-wrap">
-                                    <h2>Today's Menu</h2>
+                        <div className="clearfix mb-5 pb-5">
+                            <div className="container-fluid">
+                                <div className="row" data-aos="fade">
+                                    <div className="col-md-12 text-center heading-wrap">
+                                        <h2>Today's Menu</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="container">
+                        <div className="container">
 
-                        <div className="row no-gutters">
-                            <div className="col-md-6">
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image order-2" style={{ backgroundImage: `url('img/dishes_4.jpg')`}} data-aos="fade"></div>
-                                    <div className="text order-1">
-                                        <h3>Grilled Caesar salad, shaved reggiano</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$12.00</p>
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image order-2"
+                                             style={{backgroundImage: `url('img/dishes_4.jpg')`}} data-aos="fade"></div>
+                                        <div className="text order-1">
+                                            <h3>Grilled Caesar salad, shaved reggiano</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$12.00</p>
+                                        </div>
+
+                                    </div>
+
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image" style={{backgroundImage: `url('img/dishes_1.jpg')`}}
+                                             data-aos="fade"></div>
+                                        <div className="text">
+                                            <h3>Spicy Calamari and beans</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$12.00</p>
+
+                                        </div>
+
                                     </div>
 
                                 </div>
 
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image" style={{ backgroundImage: `url('img/dishes_1.jpg')` }} data-aos="fade"></div>
-                                    <div className="text">
-                                        <h3>Spicy Calamari and beans</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$12.00</p>
+                                <div className="col-md-6">
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image order-2"
+                                             style={{backgroundImage: `url('img/dishes_2.jpg')`}} data-aos="fade"></div>
+                                        <div className="text order-1">
+                                            <h3>Bacon wrapped wild gulf prawns</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$18.00</p>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image" style={{backgroundImage: `url('img/dishes_3.jpg')`}}
+                                             data-aos="fade"></div>
+                                        <div className="text">
+                                            <h3>Seared ahi tuna fillet*, honey-ginger sauce</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$16.00</p>
+
+                                        </div>
 
                                     </div>
 
                                 </div>
-
                             </div>
 
-                            <div className="col-md-6">
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image order-2" style={{ backgroundImage: `url('img/dishes_2.jpg')`}} data-aos="fade"></div>
-                                    <div className="text order-1">
-                                        <h3>Bacon wrapped wild gulf prawns</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$18.00</p>
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image order-2"
+                                             style={{backgroundImage: `url('img/dishes_4.jpg')`}} data-aos="fade"></div>
+                                        <div className="text order-1">
+                                            <h3>Grilled Caesar salad, shaved reggiano</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$12.00</p>
+                                        </div>
+
+                                    </div>
+
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image" style={{backgroundImage: `url('img/dishes_1.jpg')`}}
+                                             data-aos="fade"></div>
+                                        <div className="text">
+                                            <h3>Spicy Calamari and beans</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$12.00</p>
+
+                                        </div>
 
                                     </div>
 
                                 </div>
 
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image" style={{ backgroundImage: `url('img/dishes_3.jpg')`}} data-aos="fade"></div>
-                                    <div className="text">
-                                        <h3>Seared ahi tuna fillet*, honey-ginger sauce</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$16.00</p>
+                                <div className="col-md-6">
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image order-2"
+                                             style={{backgroundImage: `url('img/dishes_2.jpg')`}} data-aos="fade"></div>
+                                        <div className="text order-1">
+                                            <h3>Bacon wrapped wild gulf prawns</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$18.00</p>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div className="sched d-block d-lg-flex">
+                                        <div className="bg-image" style={{backgroundImage: `url('img/dishes_3.jpg')`}}
+                                             data-aos="fade"></div>
+                                        <div className="text">
+                                            <h3>Seared ahi tuna fillet*, honey-ginger sauce</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
+                                                delectus...</p>
+                                            <p className="text-primary h3">$16.00</p>
+
+                                        </div>
 
                                     </div>
 
                                 </div>
-
                             </div>
+
+
                         </div>
-
-                        <div className="row no-gutters">
-                            <div className="col-md-6">
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image order-2" style={{ backgroundImage: `url('img/dishes_4.jpg')`}} data-aos="fade"></div>
-                                    <div className="text order-1">
-                                        <h3>Grilled Caesar salad, shaved reggiano</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$12.00</p>
-                                    </div>
-
-                                </div>
-
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image" style={{ backgroundImage: `url('img/dishes_1.jpg')`}} data-aos="fade"></div>
-                                    <div className="text">
-                                        <h3>Spicy Calamari and beans</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$12.00</p>
-
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-                            <div className="col-md-6">
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image order-2" style={{ backgroundImage: `url('img/dishes_2.jpg')`}} data-aos="fade"></div>
-                                    <div className="text order-1">
-                                        <h3>Bacon wrapped wild gulf prawns</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$18.00</p>
-
-                                    </div>
-
-                                </div>
-
-                                <div className="sched d-block d-lg-flex">
-                                    <div className="bg-image" style={{ backgroundImage: `url('img/dishes_3.jpg')`}} data-aos="fade"></div>
-                                    <div className="text">
-                                        <h3>Seared ahi tuna fillet*, honey-ginger sauce</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto illo
-                                            delectus...</p>
-                                        <p className="text-primary h3">$16.00</p>
-
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        </div>
-
-
-                    </div>
-                </section>
+                    </section>
+                    : null
+                }
                 {/* 메뉴선택 결과 [e] */}
 
             </div>
